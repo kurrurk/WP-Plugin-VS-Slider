6deb9c52c0ae2f9338c50b08e064970ec510847a
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-03 22:15:24 +0100

    01. Structuring the plugin

---
+++ vs-slider.php
@@ -0,0 +1,34 @@
+<?php 
+
+    /** 
+    *  Plugin Name: VS Slider
+    *  Plugin URI: https://example.com/vs-slider
+    *  Description: A simple slider plugin for WordPress.
+    *  Version: 1.0.0
+    *  Requires at least: 5.6
+    *  Author: Vasily Shatalkin 
+    *  Author URI: https://example.com
+    *  License: GPL2 of later
+    *  License URI: https://www.gnu.org/licenses/gpl-2.0.html
+    *  Text Domain: vs-slider
+    *  Domain Path: /languages
+    */
+
+    /*
+        VS Slider is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 2 of the License, or
+        any later version.
+
+        VS Slider is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with VS Slider. If not, see {URI to Plugin License}.
+    */
+
+    if ( ! defined( 'ABSPATH' ) ) {
+        exit; // Exit if accessed directly.
+    }
