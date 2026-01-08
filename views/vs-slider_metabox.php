ba5f8c207acc9ba79ae4f19026eab6e0811f30c2
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-08 12:56:34 +0100

    10. Metabox API - Escaping data

--- views/vs-slider_metabox.php
+++ views/vs-slider_metabox.php
@@ -1,3 +1,9 @@
+<?php 
+
+    //$meta = get_post_meta($post->ID);
+    $link_text = get_post_meta( $post->ID, 'vs-slider_link_text', true );
+    $link_url = get_post_meta( $post->ID, 'vs-slider_link_url', true );
+?>
 <table class="form-table mv-slider-metabox">
     <tr>
         <th>
@@ -9,7 +15,7 @@
                 name="vs-slider_link_text" 
                 id="vs-slider_link_text" 
                 class="regular-text link-text"
-                value=""
+                value="<?= (isset($link_text)) ? esc_html($link_text) : ''; ?>"
                 required
             >
         </td>
@@ -24,7 +30,7 @@
                 name="vs-slider_link_url" 
                 id="vs-slider_link_url" 
                 class="regular-text link-url"
-                value=""
+                value="<?= (isset($link_url)) ? esc_url($link_url) : ''; ?>"
                 required
             >
         </td>
