0f75e3f222b30b37f95b6765fd8539323a51428a
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-08 19:41:11 +0100

    19. Settings + Options API - Splitting  sections into tabs

--- views/settings-page.php
+++ views/settings-page.php
@@ -1,10 +1,21 @@
 <div class="wrap">
     <h1><?= esc_html(get_admin_page_title()); ?></h1>
+    <?php 
+        $activate_tab = isset( $_GET['tab'] ) ? $_GET['tab'] : 'main_options';
+    ?>
+    <h2 class="nav-tab-wrapper">
+        <a href="?page=vs-slider-admin&tab=main_options" class="nav-tab <?= $activate_tab == 'main_options' ? 'nav-tab-active' : '' ?>">Main Options</a>
+        <a href="?page=vs-slider-admin&tab=additional_options" class="nav-tab <?= $activate_tab == 'additional_options' ? 'nav-tab-active' : '' ?>">Additional Options</a>
+    </h2>
     <form action="options.php" method="post">
         <?php 
-            settings_fields('vs_slider_group');
-            do_settings_sections('vs-slider-page1');
-            do_settings_sections('vs-slider-page2');
+            if ( $activate_tab == 'main_options' ) {
+                settings_fields('vs_slider_group');
+                do_settings_sections('vs-slider-page1');
+            } else {
+                settings_fields('vs_slider_group');
+                do_settings_sections('vs-slider-page2');
+            }
             submit_button('Save Settings');
         ?>
     </form>
